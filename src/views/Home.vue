8856cfa0f972e329bb62b1b47252d7a8e8931f0c
Виталий <vitalijshonov@gmail.com>
2019-08-17 15:06:28 +0300

    setting project, addind dependencies

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  .home
+    img(alt="Vue logo" src="@/assets/logo.png")
+</template>
+
+<script>
+export default {
+  name: 'home',
+}
+</script>
+
+<style lang="scss"></style>
